1ee6f9e1d20ce45e543448a898b097e9a4a5f586
Kay <duddnr654@naver.com>
2023-02-03 21:54:40 +0900

    [FEAT]: promise 구현중

--- src/refactoring/Kay/promise.js
+++ src/refactoring/Kay/promise.js
@@ -1,59 +1,108 @@
-class Promise {
+export default class Promise {
+  #value = null;
+  #state = "pending";
+  #child = null;
+  #onFulfilledCallbacks = [];
+  #onRejectedCallbacks = [];
+  #onFinallyCallbacks = [];
+
   constructor(callback) {
-    this.state = "pending";
-    this.onFulfilledCallback = null;
-    this.onRejectedCallback = null;
-
-    const resolve = (value) => {
-      this.state = "fulfilled";
-      this.value = value;
-
-      if (this.onFulfilledCallback !== null) {
-        this.onFulfilledCallback(value);
-      }
-    };
-
-    const reject = (value) => {
-      this.state = "rejected";
-      this.value = value;
-
-      if (this.onRejectedCallback !== null) {
-        this.onRejectedCallback(value);
-      }
-    };
-
-    callback(resolve, reject);
+    callback(this.#resolve, this.#reject);
   }
 
+  #resolve = (value) => {
+    if (this.#state === "pending") {
+      this.#state = "fulfilled";
+      this.#value = value;
+      this.#onFinallyCallbacks.forEach((callback) => callback());
+      if (this.#onFulfilledCallbacks.length !== 0) {
+        this.#onFulfilledCallbacks.forEach((callback) => callback(value));
+      } else {
+        this.#child?.#resolve(value);
+      }
+    }
+  };
+
+  #reject = (value) => {
+    if (this.#state === "pending") {
+      this.#state = "rejected";
+      this.#value = value;
+      this.#onFinallyCallbacks.forEach((callback) => callback());
+      if (this.#onRejectedCallbacks.length !== 0) {
+        this.#onRejectedCallbacks.forEach((callback) => callback(value));
+      } else {
+        this.#child?.#reject(value);
+      }
+    }
+  };
+
   then(callback) {
-    if (this.state === "pending") {
-      this.onFulfilledCallback = callback;
-    }
-
-    if (this.state === "fulfilled") {
-      callback(this.value);
-    }
-
-    return this;
+    this.#child = new Promise((resolve, reject) => {
+      if (this.#state === "pending") {
+        this.#onFulfilledCallbacks.push(() => {
+          this.#handleCallback(callback, resolve, reject);
+        });
+      }
+      if (this.#state === "fulfilled") {
+        this.#handleCallback(callback, resolve, reject);
+      }
+      if (this.#state === "rejected") {
+        reject(this.#value);
+      }
+    });
+    return this.#child;
   }
 
   catch(callback) {
-    if (this.state === "pending") {
-      this.onRejectedCallback = callback;
-    }
+    this.#child = new Promise((resolve, reject) => {
+      if (this.#state === "pending") {
+        this.#onRejectedCallbacks.push(() => {
+          this.#handleCallback(callback, resolve, reject);
+        });
+      }
+      if (this.#state === "rejected") {
+        this.#handleCallback(callback, resolve, reject);
+      }
+      if (this.#state === "fulfilled") {
+        resolve(this.#value);
+      }
+    });
+    return this.#child;
+  }
 
-    if (this.state === "rejected") {
-      callback(this.value);
-    }
+  finally(callback) {
+    this.#child = new Promise((resolve, reject) => {
+      if (this.#state === "pending") {
+        this.#onFinallyCallbacks.push(() => {
+          this.#handleCallback(callback, resolve, reject);
+        });
+      }
+      if (this.#state === "fulfilled" || this.#state === "rejected") {
+        this.#handleCallback(callback, resolve, reject);
+      }
+    });
+    return this.#child;
+  }
 
-    return this;
+  #handleCallback(callback, resolve, reject) {
+    try {
+      const result = callback(this.#value);
+      if (result instanceof Promise) {
+        if (result.#state === "fulfilled") {
+          result.then(resolve);
+        }
+        if (result.#state === "rejected") {
+          result.catch(reject);
+        }
+        if (result.#state === "pending") {
+          result.#onFulfilledCallbacks.push(() => result.then(resolve));
+          result.#onRejectedCallbacks.push(() => result.catch(reject));
+        }
+      } else {
+        resolve(result);
+      }
+    } catch (error) {
+      reject(error);
+    }
   }
 }
-
-function myResolve() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve("my resolve"), 1000);
-  });
-}
-
-myResolve().then((result) => console.log(result));
